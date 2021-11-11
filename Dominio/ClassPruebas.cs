b3c32d3421d413381fdf5f54441b9f5206788624
juanjosggarcia <JuanJose.Garcia11@alu.uclm.es>
2021-11-11 10:00:20 +0100

    v 0.0.0

---
+++ Dominio/ClassPruebas.cs
@@ -0,0 +1,34 @@
+﻿using Protectora.Persistencia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Protectora.Dominio
+{
+    class ClassPruebas
+    {
+        public static void Main(string[] args)
+        {
+            Perro perro = new Perro();
+            AgenteDB agente = AgenteDB.obtenerAgente();
+
+            agente.conectar();
+
+            List<List<String>> arrayUsuarios = new List<List<String>>();
+
+            arrayUsuarios = agente.leer("SELECT * FROM usuarios");
+
+
+            foreach (List<String> user in arrayUsuarios)
+            {
+                foreach (object item in user)
+                {
+                    Console.Write(item + " ");
+                }
+
+            }
+        }
+    }
+}
---
+++ Persistencia/AgenteDB.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.OleDb;
+
+
+namespace Protectora.Persistencia
+{
+    public class AgenteDB
+    {
+        private static AgenteDB AgenteBD = null;
+        private static OleDbConnection conexionBD = new OleDbConnection();
+        //private static string rutaBD = @".\protectoraDB.accdb";
+        private static string rutaBD = @"C:\Users\juanj\vs2019-workspace\Protectora\protectoraDB.accdb";
+        private string cadenaConexionBD = "Provider=Microsoft.ACE.OLEDB.12.0; Data Source=";
+
+        private AgenteDB()
+        {
+            conexionBD = new OleDbConnection();
+            conexionBD.ConnectionString = cadenaConexionBD + rutaBD;
+        }
+
+        public static AgenteDB obtenerAgente()
+        {
+            if (AgenteBD == null)
+            {
+                AgenteBD = new AgenteDB();
+            }
+            return AgenteBD;
+        }
+        public bool conectar()
+        {
+            try
+            {
+                conexionBD.Open();
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        /*
+        public void conectarCutre()
+        {
+            if (conexionBD.State == ConnectionState.Closed)
+            {
+                conexionBD.Open();
+            }
+        }
+        */
+        public void desconectar()
+        {
+            if (conexionBD.State == ConnectionState.Open)
+            {
+                conexionBD.Close();
+            }
+        }
+
+        public List<List<String>> leer(string sql)
+        {
+            List<List<String>> result = new List<List<String>>();
+            List<String> fila;
+            OleDbDataReader reader;
+            OleDbCommand com = new OleDbCommand(sql, conexionBD);
+            conectar();
+            //conectarCutre();
+
+            reader = com.ExecuteReader();
+            while (reader.Read())
+            {
+                fila = new List<String>();
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    fila.Add(reader[i].ToString());
+                }
+                result.Add(fila);
+            }
+            desconectar();
+            return result;
+        }
+
+        public int modificar(string sql)
+        {
+            OleDbCommand com = new OleDbCommand(sql, conexionBD);
+            int result;
+            conectar();
+            //conectarCutre();
+            result = com.ExecuteNonQuery();
+            desconectar();
+            return result;
+        }
+
+    }
+
+
+}
+
