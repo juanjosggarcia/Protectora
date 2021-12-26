77bc69d56c4bb1ddcabf93a716b0279c774e0edb
Lauramc13 <laura.morales8@alu.uclm.es>
2021-12-26 19:05:17 +0100

    v 0.4.11 actualizada pagina socios

--- Presentacion/PaginaSocios.xaml.cs
+++ Presentacion/PaginaSocios.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Protectora.Dominio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,42 @@ namespace Protectora.Presentacion
     /// </summary>
     public partial class PaginaSocios : Page
     {
+        public List<Socio> listaSocio = new List<Socio>();
         public PaginaSocios()
         {
             InitializeComponent();
         }
+
+        private void BtnNextSocio_Click(object sender, RoutedEventArgs e)
+        {
+            if (ListViewSocios.SelectedIndex != ListViewSocios.Items.Count - 1)
+            {
+                ListViewSocios.SelectedIndex++;
+            }
+        }
+
+        private void BtnAnteriorSocio_Click(object sender, RoutedEventArgs e)
+        {
+            if (ListViewSocios.SelectedIndex != 0)
+            {
+                ListViewSocios.SelectedIndex--;
+            }
+        }
+        private void ListaSocios_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedItems = ListViewSocios.SelectedItems;
+            foreach (Socio socio in selectedItems)
+            {
+                //SetSocio(perro);
+                //DesactivarTextBoxs();
+
+            }
+
+            btnAnteriorSocio.IsEnabled = true;
+            btnNextSocio.IsEnabled = true;
+            btnEditSocio.IsEnabled = true;
+            btnDeleteSocio.IsEnabled = true;
+        }
+
     }
 }
